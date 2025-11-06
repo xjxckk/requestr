391bc8c38458d453006364b21c766639d6d158d9
xJxckk <your.email@example.com>
2025-11-06 15:28:22 +0800

    Updates

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 setup(
     name = 'python-requestr',
     packages = ['requestr',],
-    install_requires=['python-objectifier', 'beautifulsoup4', 'requests-futures'],
-    version = '0.13',
+    install_requires=['requests', 'python-objectifier', 'beautifulsoup4', 'requests-futures'],
+    version = '0.14',
     description = 'requestr',
     url = 'https://github.com/xjxckk/python-requestr/',
     download_url = 'https://github.com/xjxckk/python-requestr/archive/refs/tags/v0.1.tar.gz',
